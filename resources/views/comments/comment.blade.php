8150d6e81bda02a18b081ba13d4c9970fa76ae0b
anzichen <zhoupengmailbox@sina.cn>
2018-11-21 17:45:08 +0800

    功能更新

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">
-    <h5><span style="color:#31b0d5">{{$comment->owner->name}}</span>:</h5>
+    <h5><span style="color:#31b0d5">{{$comment->owner->nickname}}</span>:</h5>
     <h5>{{$comment->body}}</h5>
 
     @include('comments.form',['parentId'=>$comment->id])

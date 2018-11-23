aa369eed5044fb848cd4349de4aeb5a4ceb00990
anzichen <zhoupengmailbox@sina.cn>
2018-11-23 17:17:13 +0800

    界面更新

--- resources/views/vote/detail.blade.php
+++ resources/views/vote/detail.blade.php
@@ -116,6 +116,30 @@
                     margin: 0px;
                     padding: 0px;
                 }
+
+
+                .top:after{
+            position: absolute;
+            left: -100%;                    /*改变left的值，让其相对top影藏*/
+            top: 0;
+            width: 30%;
+            height: 100%;
+            content: "";
+            /* Safari 5.1 - 6.0 */
+          background: -webkit-linear-gradient(left,rgba(255,255,255,0) 0,rgba(255,255,255,.3) 50%,rgba(255,255,255,0) 100%);
+          /* Opera 11.1 - 12.0 */
+          background: -o-linear-gradient(left,rgba(255,255,255,0) 0,rgba(255,255,255,.3) 50%,rgba(255,255,255,0) 100%);
+          /* Firefox 3.6 - 15 */
+          background: -moz-linear-gradient(left,rgba(255,255,255,0) 0,rgba(255,255,255,.3) 50%,rgba(255,255,255,0) 100%);
+          /* 标准的语法 */
+          background: linear-gradient(to right,rgba(255,255,255,0) 0,rgba(255,255,255,.3) 50%,rgba(255,255,255,0) 100%);
+        transform: skewX(-45deg);
+        }
+        .top:hover:after{
+            left: 150%;
+        transition: 1s ease;
+        }
+
                         .top{
                         z-index: 5;
                         }
